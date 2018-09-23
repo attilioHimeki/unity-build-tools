2cc15c4cd4f1dd65235b727ca99609a848941a49
DESKTOP-CVQ6K9K\PC-LISBON <carotenuto.attilio@gmail.com>
2018-09-23 14:19:23 +0100

    Folder structure and do not serialise gui fields

---
+++ Assets/Editor/BuildTools/Scripts/BuildSetupEntry.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[Serializable]
+public class BuildSetupEntry
+{
+	public string buildName = "";
+	public BuildTarget target;
+	public bool debugBuild = false;
+	public string scriptingDefineSymbols = "";
+	public bool useDefaultBuildScenes = true;
+	public List<string> customScenes;
+
+	// Advanced Options
+	public string assetBundleManifestPath = "";
+	public bool strictMode = false;
+
+	// GUI status
+	[NonSerialized] public bool guiShowCustomScenes = false;
+	[NonSerialized] public bool guiShowAdvancedOptions = false;
+}
